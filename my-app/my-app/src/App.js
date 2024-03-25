c918f0d6882c7f938edabfbfd5cbee1c86a746c4
CarlosRussell13 <russe536@msu.edu>
2024-03-25 15:04:55 -0400

    frixed

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey Mr. Siarto, thanks for reviewing my work.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
