078f6c1c7ebd63ab4d95d21b0587394f6b86a12f
CarlosRussell13 <russe536@msu.edu>
2024-03-25 15:09:25 -0400

    hello-react

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey Mr. Siarto, Thanks for reviewing my work.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Have a good day
         </a>
       </header>
     </div>
